cd036314e321426143d580c15b838214d05ccf68
brteo <matteo@meblabs.com>
2023-01-05 18:17:13 +0100

    feat: hotelier access

--- README.md
+++ README.md
@@ -8,7 +8,12 @@ If clone main repo [Ecotrip](https://github.com/eco-trip/Ecotrip) follow it's in
 
 [http://localhost:3000](http://localhost:3000)
 
+## Access
+
+You can access to control panel as **admin** with user created by [Cognito Repo](https://github.com/eco-trip/Cognito) seed.
+
+To access as **hotelier**, you must create a user for hotel with the admin access, go to AWS Cognito control panel to validate it and then you'll can log in.
+
 ## References
 
-- [awesome react components](https://github.com/brillout/awesome-react-components)
 - [antd less variables](https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less)
--- src/routes/Hotel.js
+++ src/routes/Hotel.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams, useLocation, useNavigate } from 'react-router-dom';
 import { Tabs } from 'antd';
@@ -8,6 +8,7 @@ import { faCircleInfo, faLayerGroup, faUser } from '@fortawesome/pro-solid-svg-i
 
 import ContentPanel, { renderTabBar } from '../components/layout/ContentPanel';
 import Api from '../helpers/Api';
+import AppContext from '../helpers/AppContext';
 
 import HotelDetails from './HotelDetails';
 import HotelUser from './HotelUser';
@@ -19,12 +20,15 @@ const TabKeys = {
 	Rooms: '2'
 };
 
-const Hotel = () => {
+const Hotel = ({ id }) => {
 	const { t } = useTranslation();
-	const { id: hotelId } = useParams();
+	const { logged } = useContext(AppContext);
+	const { id: paramsId } = useParams();
 	const location = useLocation();
 	const { hash: fragment } = useLocation();
 	const navigate = useNavigate();
+
+	const hotelId = paramsId || id;
 
 	const [selectedTab, setSelectedTab] = useState(null);
 
@@ -81,8 +85,15 @@ const Hotel = () => {
 		}
 	];
 
+	const tabs = logged['custom:role'] === 'hotelier' ? items.filter(item => item.key !== '1') : items;
+
 	return (
-		<ContentPanel title={hotel ? hotel.name : ''} back={() => navigate(`/hotels`)} loading={loading} withTabs>
+		<ContentPanel
+			title={hotel ? hotel.name : ''}
+			back={logged['custom:role'] === 'hotelier' ? false : () => navigate(`/hotels`)}
+			loading={loading}
+			withTabs
+		>
 			<Tabs
 				defaultActiveKey={TabKeys.General}
 				activeKey={selectedTab}
@@ -91,7 +102,7 @@ const Hotel = () => {
 					navigate(`${location.pathname}${location.search}#${e}`);
 				}}
 				renderTabBar={renderTabBar}
-				items={items}
+				items={tabs}
 			/>
 		</ContentPanel>
 	);
